42b7985061853780c0d27590828d485fe2cb53ab
Estefany <estefany.espinoza.h@gmail.com>
2022-04-05 17:04:26 -0300

    main

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import Home from '../pages/Home'
 import UserPage from '../pages/UserPage'
-import { Route, Routes } from 'react-router-dom'
+import LoginPage from '../pages/LoginPage'
 import Movements from '../pages/Movements'
+import { Route, Routes } from 'react-router-dom'
 
 const Main = () => {
   return (
@@ -10,6 +11,7 @@ const Main = () => {
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/user' element={<UserPage />} />
+        <Route path='/login' element={<LoginPage />} />
         <Route path='/movimientos' element={<Movements />} />
       </Routes>
     </main>
