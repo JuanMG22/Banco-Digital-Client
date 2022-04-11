9d3250ae8734ea416a4af5a75e7f910bd887626f
Estefany <estefany.espinoza.h@gmail.com>
2022-04-11 20:01:52 -0300

    maqueta HomeUser ok

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,25 @@
 import React from 'react'
+import TitleHome from '../components/TitleHome'
+import SubTitleHome from '../components/SubTitleHome'
+import home from '../img/home-user.jpg'
+import Cards from '../components/Cards'
 
 const Home = () => {
   return (
-    <div>
-      <h1>Home</h1>
-    </div>
+    <section className='container mx-auto'>
+      <div className='grid grid-cols-1 md:grid-cols-2 gap-1 items-center py-16'>
+        <div className='col-span-1 row-span-1 lg:mt-10'><TitleHome>Bienvenido a tu banco </TitleHome></div>
+
+        <div className='col-span-1 row-start-2 pr-9'><SubTitleHome>Revisa aqui toda la información de tu cuenta </SubTitleHome></div>
+
+        <div className='md:row-start-1 row-end-4'>
+          <img className='min-w-full md:min-w-0 my-6 lg:min-h-full max-w-xl' src={home} alt='home_user' />
+        </div>
+
+      </div>
+      <Cards />
+
+    </section>
   )
 }
 
