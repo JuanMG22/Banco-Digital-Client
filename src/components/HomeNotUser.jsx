45a8dca0fb197f1772b7a022f93838cac4b1fe14
Estefany <estefany.espinoza.h@gmail.com>
2022-04-18 19:43:31 -0300

    creacion de componentes en homeNotUser

---
+++ src/components/HomeNotUser.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import TitleHome from './TitleHome'
+import SubTitleHome from './SubTitleHome'
+import home from '../img/home-01.jpg'
+import StepsHome from './StepsHome'
+import BtnSm from './BtnSm'
+
+function HomeNotUser () {
+  return (
+    <section>
+      <div className='grid grid-cols-1 md:grid-cols-2 gap-1 justify-center py-16'>
+        <div className='col-span-1 row-span-1 lg:mt-10 pl-10'><TitleHome>Tu cuenta digital practica y sin costos</TitleHome></div>
+
+        <div className='col-span-1 row-start-2 pl-10'><SubTitleHome>Unete a Banco Digital </SubTitleHome>
+          <BtnSm>Registro</BtnSm>
+        </div>
+
+        <div className='md:row-start-1 row-end-4'>
+          <img className='min-w-full md:min-w-0 my-6 lg:min-h-full max-w-xl' src={home} alt='home' />
+        </div>
+
+      </div>
+      <StepsHome />
+
+    </section>
+  )
+}
+
+export default HomeNotUser
---
+++ src/components/StepsHome.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function StepsHome () {
+  return (
+    <div>
+      <div className='bg-blue-600 h-36 w-full'>
+        <div>
+          <div className='bg-gray-400'><p>Paso 1</p></div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default StepsHome
